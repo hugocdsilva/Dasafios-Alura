600802044f6418aaf5c39fe2ae04b627eed63ff1
Hugo Silva <hugocorreia1997@gmail.com>
2024-01-20 16:36:50 -0300

    Desafio I

---
+++ Desafio Lógica de Progranação I/logica.js
@@ -0,0 +1,23 @@
+alert('Boas vindas ao nosso site!');
+
+let nome = "Lua";
+
+let idade = 25;
+
+let numeroDeVendas = 50;
+
+let saldoDisponivel = 1000;
+
+alert('Erro!Preencha todos os campos');
+
+let mensagemDeErro = 'Erro!Preencha todos os campos';
+alert(mensagemDeErro);
+
+nome = prompt ('Digite seu nome');
+
+idade = prompt('Digite sua idade');
+
+if(idade >= 18){
+alert('Pode tirar a habilitação!')
+};
+
