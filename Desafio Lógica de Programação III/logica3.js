bf06d725a8be2c7a38be32b85b5d827d127e85fb
Hugo Silva <hugocorreia1997@gmail.com>
2024-01-22 22:42:53 -0300

    Desafio 3

---
+++ Desafio Lógica de Programação III/logica3.js
@@ -0,0 +1,42 @@
+
+//1. Contador de 1 à 10.
+let contador = 1;
+
+while(contador <= 10){
+ console.log(contador);
+ contador++;
+}
+
+/*2. Contador de 10 à 0.
+let contador = 10;
+
+while(contador >= 0){
+ alert(contador);
+ contador--
+}
+*/
+
+/*3. Contagem regressiva 0 à número escolhido
+
+let contador = 0;
+let regressiva = prompt('Digite um valor para contagem regressiva');
+
+while(contador <= regressiva){
+ alert(regressiva);
+ regressiva--
+}
+*/
+
+/*4. Contagem regressiva 0 à número escolhido
+
+let contador = 0;
+let regressiva = prompt('Digite um valor para contagem regressiva');
+
+while(contador <= regressiva){
+ alert(contador);
+ contador++
+}
+*/
+
+
+
