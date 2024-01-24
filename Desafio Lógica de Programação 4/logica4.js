c88040d229e65075e5ab323351a18e20b46fff6e
Hugo Silva <hugocorreia1997@gmail.com>
2024-01-24 16:07:57 -0300

    Desafio4

---
+++ Desafio Lógica de Programação 4/logica4.js
@@ -0,0 +1,90 @@
+/*1. Criar um "Console.log"
+alert('Boas vindas!');
+console.log('Boas vindas!');
+*/
+
+/*2. Variável nome
+let nome = 'Hugo';
+console.log(`Boas vindas ${nome}`);
+*/
+
+/*3. Variável nome
+let nome = 'Hugo';
+alert(`Olá, ${nome}!`);
+*/
+
+/*4. Armazenar no prompt
+let linguagem = prompt('Qual linguagem de programação que você mais gosta?');
+alert(linguagem);
+*/
+
+/*5. A soma de valores.
+
+let valor1 = 9;
+let valor2 = 5;
+let resultado = valor1 + valor2;
+
+console.log(`A soma de ${valor1} e ${valor2} é igual a ${resultado}.`);
+*/
+
+/*6. A subtração de valores.
+
+let valor1 = 3;
+let valor2 = 5;
+let resultado = valor1 - valor2;
+
+console.log(`A diferença de ${valor1} e ${valor2} é igual a ${resultado}.`);
+*/  
+
+/*7. Idade se é maior ou menor
+
+let idade = prompt('Informe sua idade');
+if(idade >= 18){
+    console.log('Você é maior de idade!');
+}else{console.log('Você é menor de idade!');}
+*/
+
+/*8. Números positivos, negativos ou zero
+
+let numero = prompt('Informe um número:');
+if(numero == 0){
+    console.log('O número é zero!');
+}else{
+let resultado = numero > 0 ? 'Positivo' : 'Negativo';
+console.log(`O número é ${resultado}!`);
+}
+*/
+
+/*9. Imprimir de 0 a 10.
+
+let numeroInicial = 0;
+let numeroFinal = 11;
+
+while(numeroInicial != numeroFinal){
+    console.log(numeroInicial);
+    numeroInicial++;
+}
+*/
+
+/*10. Nota
+
+let nota = 7;
+
+if(nota >= 7){
+    console.log('Você foi aprovado!');
+}else{console.log('Você foi reprovado!');}
+*/
+
+/*11. Math.random na tela
+
+console.log(Math.random());
+*/
+
+/*12. Números aleatórios de 1 à 10
+
+console.log(parseInt(Math.random()*10+1));
+*/
+
+/*13.
+console.log(parseInt(Math.random()*1000+1));
+*/
