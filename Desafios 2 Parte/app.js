9899862baad086d620a4a8dff06ca0e907d331ed
Hugo Silva <hugocorreia1997@gmail.com>
2024-02-02 15:42:15 -0300

    continuação do desafio 2

--- Desafios 2 Parte/app.js
+++ Desafios 2 Parte/app.js
@@ -2,5 +2,23 @@ let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Hora do Desafio';
 
 function verificarConsole() {
-    console.log('O botão do console foi clicado!')
+    console.log('O botão do console foi clicado!');
+}
+
+function verificarAlert() {
+    alert('Eu amo JS');
+}
+
+
+function verificarPrompt() {
+    let cidade = prompt('Informe o nome de uma cidade que você visitou no Brasil');
+    alert(`Estive em ${cidade} e lembrei de você!`);
+}
+
+
+function verificarSoma(){
+    let numero1 = parseInt(prompt('Vamos somar 2 números\n\nInforme o primeiro número:'));
+    let numero2 = parseInt(prompt('Informe o segundo número:'));
+    let soma = numero1 + numero2;
+    alert(`A soma dos número é ${soma}`);
 }
