b6f4b65aa4858d21ee9975aa6fce6d50578e15f4
Hugo Silva <hugocorreia1997@gmail.com>
2024-02-05 20:00:51 -0300

    Atualização do desafio

---
+++ Desafios 2 Parte/Dasafio 2 parte 2/logica.js
@@ -0,0 +1,38 @@
+/*1. Criar uma função que exibe "Olá, mundo!" no console.
+
+function hello(){
+    console.log('Olá, mundo!');
+}
+console.log(hello());
+*/
+
+
+/*2. Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+
+let nome = 'Hugo'
+console.log(`Hello, ${nome} World!`);
+*/
+
+
+/*3. Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+
+function numero(valor) {
+    return valor * 2;
+    }
+let numeroVar = prompt ('Informe um número:');
+let dobro = numero(numeroVar);
+console.log(`O dobro de ${numeroVar} é ${dobro}`);
+*/
+
+
+/*4. Criar uma função que recebe três números como parâmetros e retorna a média deles. 
+
+function numero(a1, a2, a3){
+    return (a1 + a2 + a3) / 3;
+}
+a1 = parseFloat(prompt('Informe 3 números para tirar a média\n\nPrimeiro número:'));
+a2 = parseFloat(prompt('Segundo número:'));
+a3 = parseFloat(prompt('Terceiro número:'));
+let media = numero(a1, a2, a3);
+console.log(`A média dos número ${a1}, ${a2} e ${a3} é: ${media}`);
+*/
