ecba4b17fd1cf8411509ad33f885d141010acb01
Hugo Silva <hugocorreia1997@gmail.com>
2024-02-17 17:42:45 -0300

    Última atualização

---
+++ Desafios 2 Parte/Dasafio 2 parte 4/app4.js
@@ -0,0 +1,28 @@
+/*1. Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+*/
+
+/*2. Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+*/
+
+/*3. Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push('Java', 'Ruby', 'Golang');
+console.log(linguagensDeProgramacao);
+*/
+
+/*4. Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let lista = ['Huguinho', 'Zezinho', 'Luizinho'];
+console.log(lista[0]);
+*/
+
+/*5. Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+let lista = ['Huguinho', 'Zezinho', 'Luizinho'];
+console.log(lista[1]);
+*/
+
+/*6. Crie uma lista com 3 nomes e exiba no console apenas o ultimo elemento.
+let lista = ['Huguinho', 'Zezinho', 'Luizinho'];
+console.log(lista[2]);
+*/
